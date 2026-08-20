f981bcb17ff8fe905af1db7b3dd96f284eb5d8af
genuinoLuiz <luizgustavo@pitom.com.br>
2026-08-20 19:17:22 -0400

    fix: correct UI_STORAGE_KEY in ui store for consistency

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,5 +1,4 @@
 import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
